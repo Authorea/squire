70bd2b2dbf9507238d01fda7e31a761f9f73eb5e
Nate Jenkins <nate.jenkins@gmail.com>
2016-09-23 00:24:29 +0200

    modified line number calculation to take into account browser bug at transition point between two rows of word-wrapped line

--- source/CursorPosition.js
+++ source/CursorPosition.js
@@ -33,26 +33,74 @@ var numberOfLinesWithinParentBlock = function(parent){
   return numLines
 }
 
-// NATE: this is kind of tricky since there is no browser way of getting
-// the current text cursor position in xy coordinates (as far as
-// I can tell).  So instead we insert a span, figure out its line number
-// within the parent block, and figure out the parent block line number
-// within the editor.
-var firstOrLastLine = function(self){
-  var root = self._root
-  var range = self.getSelection()
-  var parentBlock = getStartBlockOfRange(range)
-  var cursorNode = self.createElement( 'SPAN', {
-            id: 'cursor-start'
-  });
-  var numLines = numberOfLines(root)
 
-  insertNodeInRange( range, cursorNode );
+// NATE: there is a nasty edge case that is hard to account for.
+// When you are on a word-wrapped line, just where the wrap occurs,
+// you are "in between" two lines.  Normally your cursor will show on
+// the second line.  The general strategy to get the cursor position
+// will insert a zero width span at that point and get the bounding
+// rect.  However this bounding rect will be wrong.  It will show the
+// cursor as being at the end of the first line (if you look at the
+// content of the div, the end of the first line and beginning of the
+// second line in a word-wrapped line are the same).  getCursorPosition
+// suffers from this bug.  It also suffers from a similar bug when the
+// range is not collapsed but happens to span multiple inline elements.
+// My solution here only returns true if a range
+// is collapsed, this is a compromise that can eventually be improved upon.
+// It increases the range to one character, moving both the start and end
+// of the range into the nextSibling (if that exists).  Getting the bounding
+// rect of this single character appears to return the proper rect.
+var firstOrLastLine = function(self){
+  var root  = self._root
+  var range = self.getSelection()
+  var sc    = range.startContainer
+  var so    = range.startOffset
+  var ec, eo
+  var parentBlock = getStartBlockOfRange(range)
+  var numLines    = numberOfLines(root)
   var parentBlockLineNumber   = getLineNumber(parentBlock, root)
-  var lineNumberWithinParent  = lineNumberWithinParentBlock(cursorNode, parentBlock)
-  var numLinesParentBlock    = numberOfLinesWithinParentBlock(parentBlock)
-  detach(cursorNode)
-  mergeInlines( range.startContainer, range );
+  var numLinesParentBlock     = numberOfLinesWithinParentBlock(parentBlock)
+
+  if(!range.collapsed){
+    return {firstLine: false, lastLine: false}
+  }
+
+  if(sc.nodeType === TEXT_NODE && so < sc.length){
+    range.setEnd(sc, so+1)
+  }
+  else{
+    var nextSibling = sc.nextSibling
+    if(nextSibling){
+      if(nextSibling.nodeName === "BR"){
+        // do nothing, cursor is at the end of the line
+      }
+      else{
+        range.setStart(nextSibling, 0)
+        range.setEnd(nextSibling, 0)
+        moveRangeBoundariesDownTree(range)
+        ec = range.endContainer
+        eo = range.endOffset
+        if(ec.nodeType === TEXT_NODE && eo < ec.length){
+          range.setEnd(ec, eo+1)
+        }
+      }
+    }
+  }
+
+  // NATE: we could probably put some sanity checks on this rect.  It
+  // should only contain one character thus the width and height should
+  // both be reasonably small.
+  var rect = range.getBoundingClientRect()
+  var nodeOffset = rect.top
+  var parentOffset = parentBlock.offsetTop
+  if(nodeOffset === parentOffset){
+    return true
+  }
+  nodeOffset = nodeOffset - parentOffset
+  var lineHeight = window.getComputedStyle(parentBlock, null)["line-height"]
+  lineHeight = parseInt(lineHeight)
+  var lineNumberWithinParent = Math.round(nodeOffset/lineHeight)
+
   if(parentBlockLineNumber === 0 && lineNumberWithinParent === 0){
     if(numLines === 1 && numLinesParentBlock === 1){
       return {firstLine: true, lastLine: true}
@@ -70,15 +118,11 @@ var firstOrLastLine = function(self){
   }
 }
 
-// NATE: if performance is an issue I'm pretty sure there are
-// faster calculations for these two functions, especially
-// isFirstLine could just check if the parent block offsetTop is
-// the same as the inserted cursorNode.
 var isFirstLine = function(self){
   var res = firstOrLastLine(self)
   return res["firstLine"]
-}
 
+}
 var isLastLine = function(self){
   var res = firstOrLastLine(self)
   return res["lastLine"]
