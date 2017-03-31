8175145d94d51384d021379c37691a64553fc853
Deyan Ginev <deyan.ginev@gmail.com>
2017-03-31 16:48:53 -0400

    Revert "return result object instead of boolean in firstOrLastLine"

--- source/CursorPosition.js
+++ source/CursorPosition.js
@@ -117,7 +117,7 @@ var firstOrLastLine = function(self){
     var nodeOffset = rect.top
     var parentOffset = parentBlock.getBoundingClientRect().top
     if(nodeOffset === parentOffset){
-      return {firstLine: true, lastLine: true}
+      return true
     }
     nodeOffset = nodeOffset - parentOffset
     lineHeight = window.getComputedStyle(parentBlock, null)["line-height"]
