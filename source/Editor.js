f315fc9a701b2f62a53eefa78b0d7824171563b0
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-10-22 10:02:27 -0400

    stop focusing after format change

--- source/Editor.js
+++ source/Editor.js
@@ -1971,7 +1971,7 @@ proto.insertPlainText = function ( plainText, isPaste ) {
 var command = function ( method, arg, arg2 ) {
     return function () {
         this[ method ]( arg, arg2 );
-        return this.focus();
+        // return this.focus();
     };
 };
 
@@ -2016,7 +2016,7 @@ proto.changeBackgroundColor = function ( backgroundColorClass, commentId ) {
             'data-background': 'true'
         }
     })
-    return this.focus();
+    // return this.focus();
 };
 
 
@@ -2033,7 +2033,7 @@ proto.changeFontColor = function ( colorClass ) {
             'data-font-color': 'true'}
         }
     )
-    return this.focus();
+    // return this.focus();
 };
 
 
