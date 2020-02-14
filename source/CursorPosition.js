c8dd41993c0f82fb48a7d669b299a6eaae63b49a
dzgoldman <dzgoldman@wesleyan.edu>
2020-02-13 22:45:05 -0500

    add spacing

--- source/CursorPosition.js
+++ source/CursorPosition.js
@@ -51,7 +51,7 @@ var numberOfLinesWithinParentBlock = function(parent){
 // of the range into the nextSibling (if that exists).  Getting the bounding
 // rect of this single character appears to return the proper rect.
 
-var emptyDomRect = { x:0, y:0, width: 0, height: 0, top:0, right:0, bottom:0, left:0 }
+var emptyDomRect = { x: 0, y: 0, width: 0, height: 0, top: 0, right: 0, bottom: 0, left: 0 }
 
 var firstOrLastLine = function(self){
   var root  = self._root
